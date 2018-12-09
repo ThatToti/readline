e6c95e2e3f7570f017999d8ab99dbb44a699837f
ThatToti <mr.totihuang@gmail.com>
2018-12-09 14:24:41 +0800

    [+]实现 hashmap 切分生成

--- index.js
+++ index.js
@@ -5,32 +5,40 @@ const EventEmitter = require('events');
 class MyEmitter extends EventEmitter { }
 const emiter = new MyEmitter();
 
-const source = 'assets/c.txt'
+const source = 'assets/a.txt'
+const target = 'assets/b.txt'
 
 class Reader {
-  constructor() {
+  constructor(filepath, options) {
 
-    this.stream = fs.createReadStream(source, { highWaterMark: 10 })
+    const { highWaterMark } = options
+
+    this.stream = fs.createReadStream(filepath, { highWaterMark: highWaterMark })
 
     this.lineChunks = []
     this.lines = []
     this.buf = Buffer.alloc(0)
 
     this.isEnd = false
+    this.isPause = false
 
     this.stream.on('data', chunk => {
       [this.buf, this.lineChunks] = this.createLine(Buffer.concat([this.buf, chunk]), this.lineChunks)
       this.stream.pause()
 
       emiter.emit('next')
+      emiter.emit('pause')
+
     })
 
     this.stream.on('end', () => {
       this.isEnd = true
 
+
       if (this.buf.length > 0) {
         [this.buf, this.lineChunks] = this.createLine(Buffer.concat([this.buf]), this.lineChunks)
         emiter.emit('next')
+        emiter.emit('pause')
       }
     })
   }
@@ -54,16 +62,55 @@ class Reader {
     return this.createLine(buf.slice(index + 1), list)
   }
 
+  pause() {
+    return new Promise(resolve => {
+
+      var self = this
+      // debugger
+
+      if (this.isEnd && this.lineChunks.length === 0) {
+        resolve([false])
+      }
+
+      if (this.isPause && this.lineChunks.length === 0) {
+        resolve([false])
+      }
+
+      var fn = function () {
+        emiter.once('pause', () => {
+          // debugger
+          self.isPause = true
+          if (self.lineChunks.length === 0) {
+            self.stream.resume()
+            fn()
+          } else {
+            resolve(self.lineChunks)
+          }
+        })
+      }
+
+      fn()
+
+    })
+  }
 
   next() {
     return new Promise(resolve => {
+
+      if (this.isEnd && this.lineChunks.length === 0) {
+        // debugger
+        resolve([false])
+      }
+
       emiter.on('next', () => {
+        // debugger
         resolve(this.lineChunks)
       })
     })
   }
 
   async go() {
+    // debugger
     if (this.lines.length > 0) {
       return this.lines.shift()
     } else {
@@ -73,20 +120,57 @@ class Reader {
     }
   }
 
+  async poll() {
+
+    // debugger
+    if (this.lines.length > 0) {
+      // debugger
+      return this.lines.shift()
+    } else {
+      // debugger
+      this.lines = await this.pause()
+      return this.poll()
+    }
+
+  }
+
+  resume() {
+    // debugger
+    this.isEnd ? this.isPause = true : this.isPause = false
+    this.stream.resume()
+  }
+
 }
 
-var a = new Reader();
+var a = new Reader(source, { highWaterMark: 1024 });
+
 
 (async () => {
 
   let res
-  let count = 0
+  let hash
+  let i = 0
+
+
   while (res = await a.go()) {
 
-    count++
-    console.log(decoder.write(res), count)
+    let b = new Reader(target, { highWaterMark: 1024 });
+    let j = 0
+    while (hash = await b.poll()) {
+      // debugger
+      let hashmap = new Map()
+      hashmap.set(decoder.write(hash), j++)
+      console.log(hashmap)
+      b.resume()
+    }
+    // debugger
+
+
+    console.log(decoder.write(res), i++)
 
   }
+
+  console.log('run false')
 
 })();
 
